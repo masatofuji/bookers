a3e5d34bb4283f793b920f22164ab2ad219a2e4c
EC2 Default User <ec2-user@ip-172-31-19-219.ec2.internal>
2022-06-02 12:59:30 +0000

    フラッシュメッセージの実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,8 +7,9 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
-      redirect_to book_path(@book.id), notice: 'Book was successfully created.'
+      redirect_to book_path(@book.id)
     else
+      @books = Book.all
       render :index
     end
   end
@@ -26,6 +27,8 @@ class BooksController < ApplicationController
     if book.update(book_params)
       redirect_to book_path(book.id)
     else
+      @book = Book.find(params[:id])
+      @book.update(book_params)
       render :edit
     end
   end
